241d20968bded88112eff1d83938c631d3d34889
Christian Kogler <christian.kogler@interad.local>
2017-10-30 13:00:07 +0100

    Initial commit

---
+++ GridMvc/Filtering/CustomExpressionFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using GridMvc.Filtering.Types;
+
+namespace GridMvc.Filtering
+{
+    public class CustomExpressionFilter<T> : IColumnFilter<T>
+    {
+        private Expression<Func<T, string, bool>> _expression;
+        //private Expression<Func<T, GridFilterType, string, bool>> _expression;
+
+        public CustomExpressionFilter(Expression<Func<T, string, bool>> expression)
+        {
+            _expression = expression;
+        }
+
+        public IQueryable<T> ApplyFilter(IQueryable<T> items, ColumnFilterValue value)
+        {
+            var expr = ConvertExpression(_expression, value.FilterValue);
+            if (expr == null)
+                return items;
+
+            return items.Where(expr);
+        }
+
+        // thanks StackOverflow: https://stackoverflow.com/a/3530666
+        private static Expression<Func<T1, TResult>> ConvertExpression<T1, T2, TResult>(Expression<Func<T1, T2, TResult>> source, T2 argument)
+        {
+            Expression arg2 = Expression.Constant(argument, typeof(T2));
+            Expression newBody = new Rewriter(source.Parameters[1], arg2).Visit(source.Body);
+            return Expression.Lambda<Func<T1, TResult>>(newBody, source.Parameters[0]);
+        }
+
+        private class Rewriter : ExpressionVisitor
+        {
+            private readonly Expression candidate_;
+            private readonly Expression replacement_;
+
+            public Rewriter(Expression candidate, Expression replacement)
+            {
+                candidate_ = candidate;
+                replacement_ = replacement;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                return node == candidate_ ? replacement_ : base.Visit(node);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Grid.MVC
+========
+
+A fork from: https://gridmvc.codeplex.com/
+
+
+powered by [Interad - The .NET Experts](https://www.interad.at)
