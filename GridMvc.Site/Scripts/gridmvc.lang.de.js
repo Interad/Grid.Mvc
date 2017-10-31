400b58533f722ef26765605a8c4b7c43a2e95381
Christian Kogler <christiankogler@outlook.com>
2017-10-31 08:37:33 +0100

    Added German  Lang File

---
+++ GridMvc.Site/Scripts/gridmvc.lang.de.js
@@ -0,0 +1,20 @@
+﻿window.GridMvc = window.GridMvc || {};
+GridMvc.lang.de = {
+    filterTypeLabel: "Art: ",
+    filterValueLabel: "Wert:",
+    applyFilterButtonText: "Anwenden",
+    filterSelectTypes: {
+        Equals: "Gleich",
+        StartsWith: "Beginnt mit",
+        Contains: "Beinhaltet",
+        EndsWith: "Endet mit",
+        GreaterThan: "Größer als",
+        LessThan: "Kleiner als",
+        GreaterThanOrEquals: "Größer oder gleich",
+        LessThanOrEquals: "Kleiner oder gleich"
+    },
+    code: 'de',
+    boolTrueLabel: "Ja",
+    boolFalseLabel: "Nein",
+    clearFilterLabel: "Filter rücksetzen"
+};
