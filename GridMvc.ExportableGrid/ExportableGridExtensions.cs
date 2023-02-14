f7525f6ab7c5de9584eb6b996ea6ca9be15d60c4
David Prem <david.prem@interad.at>
2023-02-14 16:17:41 +0100

    Move ExportableGrid to separate project
    This removes the dependency to ClosedXML and DocumentFormat.OpenXml from
    the main project.

---
+++ GridMvc.ExportableGrid/ExportableGridExtensions.cs
@@ -0,0 +1,14 @@
+﻿using GridMvc.Html;
+using GridMvc.Utility.ExportableGrid;
+using System.Web.Mvc;
+
+namespace GridMvc.ExportableGrid
+{
+    public static class ExportableGridExtensions
+    {
+        public static HtmlGrid<T> Grid<T>(this HtmlHelper helper, ExportableGrid<T> grid) where T : class
+        {
+            return grid.AsHtmlGrid(helper);
+        }
+    }
+}
--- GridMvc.ExportableGrid/Properties/AssemblyInfo.cs
+++ GridMvc.ExportableGrid/Properties/AssemblyInfo.cs
@@ -2,17 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("GridMvc.ExportableGrid")]
-
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Interad.Grid.Mvc")]
-[assembly: AssemblyDescription("ASP.NET MVC Grid component")]
+[assembly: AssemblyTitle("Interad.Grid.Mvc.ExportableGrid")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Interad.Grid.Mvc")]
+[assembly: AssemblyProduct("Interad.Grid.Mvc.ExportableGrid")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,12 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("7d38c093-51ea-4869-8044-0a85eda1bb6f")]
+[assembly: Guid("dc41998f-6098-4f32-bde3-94948d31b5a4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -37,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
-[assembly: InternalsVisibleTo("GridMvc.Tests")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
